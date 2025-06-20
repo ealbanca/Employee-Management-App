941602b10c9936de7b15c85e90c3d135a7034e16
ealbanca <ealbanca@byui.edu>
2025-06-20 17:09:41 -0600

    Created JS file to add event listener to the main buttons

---
+++ scripts/employeeButtons.js
@@ -0,0 +1,10 @@
+// Added event listener to the employee buttons of the main page
+document.getElementById('addEmployee').onclick = function () {
+    window.location.href = '/addemployee.html';
+};
+document.getElementById('viewEmployees').onclick = function () {
+
+}
+document.getElementById('searchEmployee').onclick = function () {
+    window.location.href = '/searchemployee.html';
+}
